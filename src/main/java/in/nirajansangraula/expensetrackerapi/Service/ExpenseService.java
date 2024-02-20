0173f6a579a0f3963b88f8d636b93328ae55fcbc
Nirajan Sangraula <neerazansangraula14@gmail.com>
2024-02-20 00:24:16 -0500

    project restructure

--- src/main/java/in/nirajansangraula/expensetrackerapi/Service/ExpenseService.java
+++
@@ -1,9 +0,0 @@
-package in.nirajansangraula.expensetrackerapi.Service;
-
-import in.nirajansangraula.expensetrackerapi.entity.Expense;
-import java.util.List;
-import org.springframework.stereotype.Service;
-
-public interface ExpenseService {
-    List<Expense> getAllExpenses();
-}
